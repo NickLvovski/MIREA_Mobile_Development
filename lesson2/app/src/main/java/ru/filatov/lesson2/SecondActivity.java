e8e25061cc67a9b4200e5c5fdb9991b23032e85a
NickLvovski <nicklvovski@gmail.com>
2023-04-22 09:05:12 +0300

    lesson2

---
+++ lesson2/app/src/main/java/ru/filatov/lesson2/SecondActivity.java
@@ -0,0 +1,30 @@
+package ru.filatov.lesson2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class SecondActivity extends AppCompatActivity {
+    private EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+        editText=findViewById(R.id.editTextTextPersonName2);
+        Bundle extras = getIntent().getExtras();
+        String value = extras.getString("key");
+        editText.setText(value);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+}
